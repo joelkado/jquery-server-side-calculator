5895c9159f31344756e94b2ae990781eef75f484
Joelle Kado <jkado@my.hpu.edu>
2020-11-12 18:01:41 -0600

    UI CREATED

---
+++ server/public/scripts/client.js
@@ -0,0 +1,68 @@
+console.log('JS is CONNECTED');
+
+$(document).ready(readyNow);
+
+function readyNow() {//ENTER readyNow
+    console.log('JQ is READY');
+
+    //click handler for save
+    $('#btn-submit').on('click', addCat)
+
+}//EXIT readyNow
+
+function addCat(event) {//ENTER addCat
+    //prevent form-refresh
+    event.preventDefault();
+
+    //get name to send to server
+    let name = $('#in-name').val();
+    console.log('Adding a cat', name);
+
+    $.ajax({
+        method: 'POST',
+        url: '/cat',
+        data: {
+            cat: name
+        }
+    }).then(function (response) {
+        //then is run if we get a good response from server
+        console.log('Added Successfully');
+        //get all cats again, so we see the update 
+        getCats();
+        //clear input
+        $('#in-name').val('');
+    }).catch(function (error) {
+        //catch is run if there is a bad response from server
+        //log th error and alert the user
+        console.log('Error', error);
+        alert('Something went wrong.')
+    })
+
+}//EXIT addCat
+
+function getCats() {//ENTER getCats
+    //making a get request to our server
+    //this returns back a 'Promise'
+    $.ajax({
+        method: 'GET',
+        url: '/cat'
+    }).then(function (response) {
+        console.log('Got response:', response);
+        renderCats(response)
+    }).catch(function (error) {
+        //log th error and alert the user
+        console.log('Error', error);
+        alert('Something went wrong.')
+    })
+
+    console.log('EXIT getCats');
+}//EXIT getCats
+
+function renderCats(catArray) {//ENTER renderCats
+    console.log('Here is the catsArray we got from the server', catArray);
+    //append to the DOM
+    $('#cat-list').empty();
+    for (let cat of catArray) {
+        $('#cat-list').append(`<li>${cat}</li>`);
+    }
+}//EXIT renderCats
---
+++ server/server.js
@@ -0,0 +1,40 @@
+//bring in express from the node_modules folder
+//express is a function
+const express = require('express');
+
+//bring in body parser which will help us parse inoming data
+const bodyParser = require('body-parser');
+
+//create an instance of the express web server. we'll call it app
+const app = express();
+
+//we'll use this port later, its like a box number at the post office
+//where our server will get/send mail/mesages
+const port = 5000;
+
+//tell express where to find static files that it can send on request
+app.use(express.static('server/public'));
+
+//tell express how to parse incominge data
+app.use(bodyParser.urlencoded({extended : true}));
+
+// ----these routes & data will vary for each assignment----
+const catArray = ['Abby', 'Kyo', 'Fred'];
+//route to get marys cats
+app.get('/cat',(req, res) =>{
+    console.log('sending cat data');
+    res.send(catArray);
+})
+    app.post('/cat', (req, res) => {
+        let catData = req.body.cat;
+        console.log('Getting catdata', catData);
+        catArray.push(catData);
+        res.sendStatus(200); //200 is an ok status
+        
+    })
+    //----end of our routes-----------
+
+//tell our server to start listening for requests on our port
+app.listen(port, ()=> {
+    console.log('server is lestining on port', port);
+});
